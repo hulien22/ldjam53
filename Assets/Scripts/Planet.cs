4babdc77646a859b877823795f6c9261188de0d6
hulien22 <hejulien22@gmail.com>
2023-04-30 01:37:22 -0700

    cleanup unused planet code

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -7,19 +7,4 @@ public class Planet : MonoBehaviour
     // TODO use a parent class for all gravity bodies?
     public float gravityStrength;
     public float atmosphereDistance;
-
-    public Vector3 previousPosition;
-    public Vector2 worldVelocity;
-
-    private void Start()
-    {
-        previousPosition = transform.position;
-    }
-
-    private void FixedUpdate()
-    {
-        worldVelocity = (transform.position - previousPosition) / Time.deltaTime;
-        previousPosition = transform.position;
-        // Debug.Log(worldVelocity);
-    }
 }
