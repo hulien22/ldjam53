8dafce05502a80c14a82d47320bd81a490e831fd
hulien22 <hejulien22@gmail.com>
2023-05-01 16:39:50 -0700

    add changescenesaction

---
+++ Assets/Scripts/StorySystem/ChangeScenesAction.cs
@@ -0,0 +1,26 @@
+﻿using CleverCrow.Fluid.Dialogues.Actions;
+using CleverCrow.Fluid.Dialogues.Nodes;
+using CleverCrow.Fluid.Dialogues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using CleverCrow.Fluid.Dialogues.Actions.Databases;
+using CleverCrow.Fluid.Databases;
+using Assets.Scripts.StorySystem;
+using Unity.VisualScripting;
+
+[CreateMenu("Action/ChangeScenes")]
+public class ChangeScenesAction : ActionDataBase
+{
+    [field: SerializeField]
+    public string SceneName { get; set; }
+
+    public override ActionStatus OnUpdate()
+    {
+        GlobalState.ChangeScenes(SceneName);
+        return base.OnUpdate();
+    }
+}
